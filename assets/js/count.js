c3c1b3b86a6526483a2cc7506073502f813f3d4d
Chiho99 <chihomikami.81@gmail.com>
2020-10-29 20:34:37 +0900

    add count.js

---
+++ assets/js/count.js
@@ -0,0 +1,12 @@
+// let black = $('.black').length;
+// let white = $('.white').length;
+// $('.count_b').text(black++);
+// $('.count_w').text(white++);
+
+
+function count(){
+   let black = $('.black').length;
+   $('.count_b').text(black);
+   let white = $('.white').length;
+   $('.count_w').text(white);
+} 
